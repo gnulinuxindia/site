82bef68078ddae7837b99d3183765733c391f82d
ashirbadsahu <ashirbadsahu300@gmail.com>
2024-04-24 20:46:37 +0530

    Fix typo in author name and update date format to DDMMYY

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,7 +15,7 @@ const blog = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        authour: z.string().optional(),
+        author: z.string().optional(),
         // Transform string to Date object
         pubDate: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
